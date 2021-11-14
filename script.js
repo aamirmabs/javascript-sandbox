6e8d68081309b59aca0497224aba0d842d83690d
ogaamyr <aamir@ogaamyr.com>
2021-11-14 05:41:42 +0100

    chaining promises with nesting

--- script.js
+++ script.js
@@ -64,16 +64,17 @@
 // using a promise to emulate the request callback
 
 const fakeRequestPromise = (url) => {
+  console.log(`==========`);
   console.log(`Requested URL is: ${url}`);
 
   return new Promise((resolve, reject) => {
     const delay = Math.floor(Math.random() * 2000) + 500;
 
     setTimeout(() => {
-      console.log(delay);
-      if (delay < 1500) {
-        console.log(`Data recieved in ${delay} ms`);
+      // console.log(delay);
+      if (delay < 2000) {
         resolve(`Your data recieved from ${url}`);
+        console.log(`Data recieved in ${delay} ms`);
       } else {
         console.log(`ERR: Data not recieved`);
         reject(`Connection failure`);
@@ -82,5 +83,28 @@ const fakeRequestPromise = (url) => {
   });
 };
 
-let response = fakeRequestPromise(`url1`);
-console.log(response);
+fakeRequestPromise(`google.com`)
+  .then(() => {
+    console.log(`1: Success`);
+
+    // sending second request
+    fakeRequestPromise(`facebook.com`)
+      .then(() => {
+        console.log(`2: Success`);
+
+        // sending third request
+        fakeRequestPromise(`instagram.com`)
+          .then(() => {
+            console.log(`3: Success`);
+          })
+          .catch(() => {
+            console.log(`3: Failure`);
+          });
+      })
+      .catch(() => {
+        console.log(`2: Failure`);
+      });
+  })
+  .catch(() => {
+    console.log(`1: Failure`);
+  });
