db49cf3d6e1ffad90d62dc42df1447e3722ad87d
ogaamyr <aamir@ogaamyr.com>
2021-11-26 06:43:58 +0100

    rest api practice - comment index render with ejs + route to index file

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require(`express`);
 const path = require(`path`);
+const { v4: uuid } = require("uuid");
 
 // using express
 // ----------
 const app = express();
 const port = 3000;
+
+// serving static file assets from /public
+app.use(express.static(path.join(__dirname, `public`)));
 
 // making express use ejs as the view engine
 app.set(`view engine`, `ejs`);
@@ -42,6 +46,67 @@ app.get(`/r/:subreddit`, (req, res) => {
   const { subreddit } = req.params;
   const test = { valueA: 10, valueB: 20 };
   res.render(`subreddit`, { subreddit, test });
+});
+
+// comments with restful api
+
+// Our fake database:
+let comments = [
+  {
+    id: uuid(),
+    username: "Todd",
+    comment: "lol that is so funny!",
+  },
+  {
+    id: uuid(),
+    username: "Skyler",
+    comment: "I like to go birdwatching with my dog",
+  },
+  {
+    id: uuid(),
+    username: "Sk8erBoi",
+    comment: "Plz delete your account, Todd",
+  },
+  {
+    id: uuid(),
+    username: "onlysayswoof",
+    comment: "woof woof woof",
+  },
+];
+
+// GET /comments - list all comments
+// POST /comments/ - create a new comment
+// GET /comments/:id - get a single comment (using ID)
+// PATCH /comments/:id - update one comment
+// DELETE /comments/:id - destroy one comment
+
+// GET /comments/ - list all comments
+app.get(`/comments`, (req, res) => {
+  // res.send(`GET: /comments`);
+  res.render(`comments/index`, { allComments: comments });
+});
+
+// POST /comments/ - create a new comment
+app.post(`/comments`, (req, res) => {
+  res.send(`POST: /comments`);
+});
+
+// GET /comments/:id - get a single comment (using ID)
+app.get(`/comments/:id`, (req, res) => {
+  const { id } = req.body;
+  res.send(`: /comments/:id ${id}`);
+});
+
+// PATCH /comments/:id - update one comment
+app.patch(`/comments/:id`, (req, res) => {
+  const { id } = req.body;
+  res.send(`PATCH: /comments/:id ${id}`);
+});
+
+// DELETE /comments/:id - destroy one comment
+app.delete(`/comments/:id`, (req, res) => {
+  const { id } = req.body;
+  res.send(`DELETE: /comments/:id ${id}`);
 });
 
 // app listening to requests on port
